eaf4d698f4a7d51c7112db9bc9aae1d4cd140e3d
Jia Rong Gan <jiaronggan21@gmail.com>
2024-01-02 18:20:49 -0600

    added random function and getText function

--- markov.js
+++ markov.js
@@ -30,23 +30,37 @@ class MarkovMachine {
     let chains = {};
 
     for (let i=0; i<this.words.length; i++){
-      let nextWord = []
+      let nextWord = [];
       chains[this.words[i]] = nextWord.push(this.words[i+1] || null);
-      chains[this.words[i]] = nextWord
+      chains[this.words[i]] = nextWord;
     }
 
     return chains
   }
+  //Ask about best practice for code below. static? or
+  // regular function outside of class??
 
+  static randomChoice(words){
+    return words[Math.floor(Math.random() * words.length)]
+  }
 
   /** Return random text from chains, starting at the first word and continuing
    *  until it hits a null choice. */
 
   getText() {
     // TODO: implement this!
-
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
+
+    let result = [];
+    let startKey = this.words[0];
+
+    while (startKey !== null){
+      result.push(startKey);
+      const nextWord = randomChoice(this.chains[startKey])
+      startKey = nextWord;
+    }
+    return result.join(" ");
   }
 }
